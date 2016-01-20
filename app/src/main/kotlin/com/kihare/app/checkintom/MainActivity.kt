6783359d3032ad5e6e020d89af7f247f4d7ec130
kiharekato <kiharekato@gmail.com>
2016-01-21 01:31:55 +0900

    とりあえず書き込みまで。

--- app/src/main/kotlin/com/kihare/app/checkintom/MainActivity.kt
+++ app/src/main/kotlin/com/kihare/app/checkintom/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.kihare.app.checkintom
 
+import android.app.PendingIntent
 import android.content.Intent
-import android.nfc.NdefMessage
-import android.nfc.NdefRecord
-import android.nfc.NfcAdapter
+import android.nfc.*
+import android.nfc.tech.Ndef
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -14,6 +14,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.os.Bundle
+import android.os.Parcelable
 import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
@@ -29,12 +30,12 @@ import java.nio.charset.Charset
 
 import io.fabric.sdk.android.Fabric;
 import com.crashlytics.android.Crashlytics;
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
     private var mViewPager: ViewPager? = null
-    private val MIME_TEXT_PLAIN = "text/plain"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,40 +49,122 @@ class MainActivity : AppCompatActivity() {
 
         mViewPager = findViewById(R.id.container) as ViewPager
         mViewPager!!.adapter = mSectionsPagerAdapter
+    }
 
+    private final val SMART_POSTER_URL = "http://otakumode.com/"
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        val tag = (intent?.getParcelableExtra<Parcelable>(NfcAdapter.EXTRA_TAG) as? Tag) ?: return
+        val ndefTag = Ndef.get(tag) ?: return
+
+        val message = createSmartPoster(SMART_POSTER_URL)
+        if (writeNdefMessage(ndefTag, message)) {
+            Toast.makeText(this, "URLを書き込みました。", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "書き込みに失敗しました。", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun createSmartPoster(url:String):NdefMessage {
+        val rs = arrayOf(createUriRecord(url), createActionRecord())
+        val spPayload = NdefMessage(rs)
+        val spRecord = NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_SMART_POSTER, ByteArray(0), spPayload.toByteArray())
+        return NdefMessage(arrayOf(spRecord))
+    }
+
+    private fun createUriRecord(url:String):NdefRecord {
+        return NdefRecord.createUri(url)
+    }
+
+    private fun  createActionRecord(): NdefRecord {
+        val typeField = "act".toByteArray(Charset.forName("US-ASCII"))
+        val payload = byteArrayOf(0x00.toByte())
+        return NdefRecord(NdefRecord.TNF_WELL_KNOWN, typeField, null, payload)
+    }
+
+    private fun writeNdefMessage(ndefTag: Ndef, ndefMessage: NdefMessage): Boolean {
+        if (!ndefTag.isWritable) return false
+
+        val messageSize = ndefMessage.toByteArray().size
+        if (messageSize > ndefTag.maxSize) return false
+
+        try {
+            if (!ndefTag.isConnected) {
+                ndefTag.connect()
+            }
+            ndefTag.writeNdefMessage(ndefMessage)
+            return true;
+        } catch (e: TagLostException ) {
+            return false;
+        } catch (e: IOException) {
+            return false;
+        } catch (e: FormatException) {
+            return false;
+        } finally {
+            try {
+                ndefTag.close()
+            } catch (e: IOException) {
+            }
+        }
     }
 
     class PlaceholderFragment : Fragment() {
-        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-            val rootView = inflater!!.inflate(R.layout.fragment_main, container, false)
-            if(arguments.getInt(ARG_SECTION_NUMBER) != 0) return rootView
+        private var nfcAdapter: NfcAdapter? = null
 
-            var nfcAdapter = NfcAdapter.getDefaultAdapter(activity)
-            if(nfcAdapter == null || !nfcAdapter.isEnabled){
+        override fun onResume() {
+            super.onResume()
+            val i = Intent(activity, MainActivity::class.java)
+            i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            val pi = PendingIntent.getActivity(activity, 0, i, 0)
+            nfcAdapter?.enableForegroundDispatch(activity, pi, null, null)
+        }
+
+        override fun onPause() {
+            super.onPause()
+            nfcAdapter?.disableForegroundDispatch(activity)
+        }
+
+        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            val rootView: View
+
+            nfcAdapter = NfcAdapter.getDefaultAdapter(activity)
+            if(nfcAdapter == null || !nfcAdapter!!.isEnabled){
                 Toast.makeText(activity, "failed", Toast.LENGTH_SHORT).show()
+                return null
             }
 
-            Log.d("NFC", "createFragment")
-            val activityIntent = activity.intent
-            if(TextUtils.equals(activityIntent.action, NfcAdapter.ACTION_TECH_DISCOVERED)){
-                val idm: String? = getIdm(activityIntent)
-                if (idm != null) {
-                    Log.d("NFC", "$idm")
-                    (rootView.findViewById(R.id.idm) as TextView).text = idm
+            if(arguments.getInt(ARG_SECTION_NUMBER) != 0) {
+                // 読み込み
+                rootView = inflater!!.inflate(R.layout.fragment_main, container, false)
+                Log.d("NFC", "createFragment")
+                val activityIntent = activity.intent
+                if (TextUtils.equals(activityIntent.action, NfcAdapter.ACTION_TECH_DISCOVERED)) {
+                    val idm: String? = getIdm(activityIntent)
+                    if (idm != null) {
+                        Log.d("NFC", "$idm")
+                        (rootView.findViewById(R.id.idm) as TextView).text = idm
+                    }
+
+                    val rawMsgs = activityIntent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+                    if (rawMsgs != null) {
+                        val msgs = arrayListOf<NdefMessage>()
+                        for (i in 0..rawMsgs.size - 1) {
+                            msgs[i] = rawMsgs[i] as NdefMessage
+                        }
+                        (rootView.findViewById(R.id.message) as TextView).text = msgs.toString()
+                    } else {
+                        Log.d("NFC", "message is null.")
+                    }
                 }
 
-                val rawMsgs = activityIntent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
-                if (rawMsgs != null) {
-                    val msgs = arrayListOf<NdefMessage>()
-                    for (i in 0..rawMsgs.size- 1) {
-                        msgs[i] = rawMsgs[i] as NdefMessage
-                    }
-                    (rootView.findViewById(R.id.message) as TextView).text = msgs.toString()
-                } else {
-                    Log.d("NFC", "message is null.")
-                }
+            } else {
+
+                rootView = inflater!!.inflate(R.layout.fragment_main2, container, false)
+
             }
             return rootView
+
         }
 
         private fun getIdm(intent: Intent): String {
