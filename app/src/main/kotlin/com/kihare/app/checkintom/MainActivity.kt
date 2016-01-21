55ea95fc93b77870d3a804b08f111777284d7304
kiharekato <kiharekato@gmail.com>
2016-01-22 03:58:40 +0900

    無理くり修正

--- app/src/main/kotlin/com/kihare/app/checkintom/MainActivity.kt
+++ app/src/main/kotlin/com/kihare/app/checkintom/MainActivity.kt
@@ -4,61 +4,42 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.nfc.*
 import android.nfc.tech.Ndef
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.view.ViewPager
 import android.os.Bundle
 import android.os.Parcelable
-import android.text.TextUtils
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-
-import android.widget.TextView
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.widget.EditText
 import android.widget.Toast
-import org.w3c.dom.Text
+import java.io.IOException
 import java.nio.charset.Charset
 
-import io.fabric.sdk.android.Fabric;
-import com.crashlytics.android.Crashlytics;
-import java.io.IOException
-
 class MainActivity : AppCompatActivity() {
-
-    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
-    private var mViewPager: ViewPager? = null
+    private var editView: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Fabric.with(this, Crashlytics())
-
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
 
-        mViewPager = findViewById(R.id.container) as ViewPager
-        mViewPager!!.adapter = mSectionsPagerAdapter
+        editView = findViewById(R.id.text) as EditText
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter == null || !(nfcAdapter as NfcAdapter).isEnabled) {
+            Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show()
+            return
+        }
     }
 
-    private final val SMART_POSTER_URL = "http://otakumode.com/"
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
 
+        // nfcからのintentかのチェック
         val tag = (intent?.getParcelableExtra<Parcelable>(NfcAdapter.EXTRA_TAG) as? Tag) ?: return
         val ndefTag = Ndef.get(tag) ?: return
 
-        val message = createSmartPoster(SMART_POSTER_URL)
+        val message = createSmartPoster() ?: return
         if (writeNdefMessage(ndefTag, message)) {
             Toast.makeText(this, "URLを書き込みました。", Toast.LENGTH_LONG).show()
         } else {
@@ -66,26 +47,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun createSmartPoster(url:String):NdefMessage {
-        val rs = arrayOf(createUriRecord(url), createActionRecord())
+    fun createSmartPoster(): NdefMessage? {
+        val typeField = "act".toByteArray(Charset.forName("US-ASCII"))
+        val payload = byteArrayOf(0x00.toByte())
+        val record = NdefRecord(NdefRecord.TNF_WELL_KNOWN, typeField, null, payload)
+
+        val name = editView!!.text ?: return null
+        val rs = arrayOf(NdefRecord.createUri(name.toString()), record)
         val spPayload = NdefMessage(rs)
         val spRecord = NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_SMART_POSTER, ByteArray(0), spPayload.toByteArray())
         return NdefMessage(arrayOf(spRecord))
     }
 
-    private fun createUriRecord(url:String):NdefRecord {
-        return NdefRecord.createUri(url)
-    }
-
-    private fun  createActionRecord(): NdefRecord {
-        val typeField = "act".toByteArray(Charset.forName("US-ASCII"))
-        val payload = byteArrayOf(0x00.toByte())
-        return NdefRecord(NdefRecord.TNF_WELL_KNOWN, typeField, null, payload)
-    }
-
     private fun writeNdefMessage(ndefTag: Ndef, ndefMessage: NdefMessage): Boolean {
         if (!ndefTag.isWritable) return false
 
+        // 書き込み可能かのチェック
         val messageSize = ndefMessage.toByteArray().size
         if (messageSize > ndefTag.maxSize) return false
 
@@ -93,9 +70,10 @@ class MainActivity : AppCompatActivity() {
             if (!ndefTag.isConnected) {
                 ndefTag.connect()
             }
+            // 書き込み
             ndefTag.writeNdefMessage(ndefMessage)
             return true;
-        } catch (e: TagLostException ) {
+        } catch (e: TagLostException) {
             return false;
         } catch (e: IOException) {
             return false;
@@ -109,105 +87,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    class PlaceholderFragment : Fragment() {
-        private var nfcAdapter: NfcAdapter? = null
-
-        override fun onResume() {
-            super.onResume()
-            val i = Intent(activity, MainActivity::class.java)
-            i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            val pi = PendingIntent.getActivity(activity, 0, i, 0)
-            nfcAdapter?.enableForegroundDispatch(activity, pi, null, null)
-        }
-
-        override fun onPause() {
-            super.onPause()
-            nfcAdapter?.disableForegroundDispatch(activity)
-        }
-
-        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-            val rootView: View
-
-            nfcAdapter = NfcAdapter.getDefaultAdapter(activity)
-            if(nfcAdapter == null || !nfcAdapter!!.isEnabled){
-                Toast.makeText(activity, "failed", Toast.LENGTH_SHORT).show()
-                return null
-            }
-
-            if(arguments.getInt(ARG_SECTION_NUMBER) != 0) {
-                // 読み込み
-                rootView = inflater!!.inflate(R.layout.fragment_main, container, false)
-                Log.d("NFC", "createFragment")
-                val activityIntent = activity.intent
-                if (TextUtils.equals(activityIntent.action, NfcAdapter.ACTION_TECH_DISCOVERED)) {
-                    val idm: String? = getIdm(activityIntent)
-                    if (idm != null) {
-                        Log.d("NFC", "$idm")
-                        (rootView.findViewById(R.id.idm) as TextView).text = idm
-                    }
-
-                    val rawMsgs = activityIntent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
-                    if (rawMsgs != null) {
-                        val msgs = arrayListOf<NdefMessage>()
-                        for (i in 0..rawMsgs.size - 1) {
-                            msgs[i] = rawMsgs[i] as NdefMessage
-                        }
-                        (rootView.findViewById(R.id.message) as TextView).text = msgs.toString()
-                    } else {
-                        Log.d("NFC", "message is null.")
-                    }
-                }
-
-            } else {
-
-                rootView = inflater!!.inflate(R.layout.fragment_main2, container, false)
-
-            }
-            return rootView
-
-        }
-
-        private fun getIdm(intent: Intent): String {
-            var idm: String = ""
-            val idmByte = StringBuffer()
-            val rawIdm = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID)
-            if (rawIdm != null) {
-                for (i in 0..rawIdm.size - 1) {
-                    idmByte.append(Integer.toHexString(rawIdm[i].toInt()))
-                }
-                idm = idmByte.toString()
-            }
-            return idm
-        }
-
-        companion object {
-            private val ARG_SECTION_NUMBER = "section_number"
-            fun newInstance(sectionNumber: Int): PlaceholderFragment {
-                val fragment = PlaceholderFragment()
-                val args = Bundle()
-                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
-                fragment.arguments = args
-                return fragment
-            }
-        }
+    private var nfcAdapter: NfcAdapter? = null
+    override fun onResume() {
+        super.onResume()
+        val i = Intent(this, MainActivity::class.java)
+        i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pi = PendingIntent.getActivity(this, 0, i, 0)
+        nfcAdapter?.enableForegroundDispatch(this, pi, null, null)
     }
 
-    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-
-        override fun getItem(position: Int): Fragment {
-            return PlaceholderFragment.newInstance(position + 1)
-        }
-
-        override fun getCount(): Int {
-            return 2
-        }
-
-        override fun getPageTitle(position: Int): CharSequence? {
-            when (position) {
-                0 -> return "SECTION 1"
-                1 -> return "SECTION 2"
-            }
-            return null
-        }
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
     }
 }
--- app/src/main/kotlin/com/kihare/app/checkintom/TwitterLoginActivity.kt
+++ app/src/main/kotlin/com/kihare/app/checkintom/TwitterLoginActivity.kt
@@ -1,35 +1,98 @@
 package com.kihare.app.checkintom
 
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NfcAdapter
+import android.os.AsyncTask
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
-import com.twitter.sdk.android.Twitter
-import com.twitter.sdk.android.core.*
-import com.twitter.sdk.android.core.identity.TwitterLoginButton
-import io.fabric.sdk.android.Fabric
+import android.util.Log
+import android.widget.Toast
+import twitter4j.TwitterFactory
+import twitter4j.conf.ConfigurationBuilder
+import java.io.UnsupportedEncodingException
 
 class TwitterLoginActivity : AppCompatActivity() {
 
+    private val TAG = "CheckInTomStudy"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val authConfig = TwitterAuthConfig("W2SZFrhkJ2MDCkH4nXMBx2ibC", "y9HmHvFtivenGmPf9NODGRW5oibkpAM1PPb78r5kdYpM3rJBaY")
-        Fabric.with(this, Twitter(authConfig))
-
         setContentView(R.layout.activity_twitter_login)
-        val loginButton = findViewById(R.id.login_button) as TwitterLoginButton
-        loginButton.callback = object : Callback<TwitterSession>() {
-            override fun success(result: Result<TwitterSession>) {
-                // Do something with result, which provides a TwitterSession for making API calls
+
+        // nfcが使える端末かのcheck
+        val nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter == null || !nfcAdapter.isEnabled) {
+            Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        AsyncHttpRequest().execute()
+    }
+
+    inner class AsyncHttpRequest() : AsyncTask<Void, Void, Void>() {
+
+        override fun doInBackground(vararg params: Void?): Void? {
+            var name = ""
+
+            // nfcデータの取得
+            val idm: String? = getIdm()
+            if (idm != null) Log.d(TAG, "Get nfc id: $idm")
+            val rawMsgs = intent!!.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            Log.d(TAG, rawMsgs.size.toString())
+            if (rawMsgs != null) {
+                if (rawMsgs.size != 0) {
+                    name = readText((rawMsgs[0] as NdefMessage).records[0])
+                }
+            } else {
+                Log.d(TAG, "message is null.")
             }
 
-            override fun failure(exception: TwitterException) {
-                // Do something on failure
+            // tweet処理
+            Log.d(TAG, name + ".")
+            try {
+                val cb = ConfigurationBuilder()
+                cb.setDebugEnabled(true)
+                        .setOAuthConsumerKey("****")
+                        .setOAuthConsumerSecret("****")
+                        .setOAuthAccessToken("****-****")
+                        .setOAuthAccessTokenSecret("****")
+
+                val tf = TwitterFactory(cb.build())
+                val twitter = tf.instance
+                twitter.verifyCredentials()
+                twitter.updateStatus("$name, test")
+            } catch(e: Exception) {
+                Log.d(TAG, e.message)
             }
+
+            return null
+        }
+
+        override fun onPostExecute(result: Void?) {
+            finish()
+        }
+
+        private fun getIdm(): String {
+            var idm: String = ""
+            val idmByte = StringBuffer()
+            val rawIdm = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID)
+            if (rawIdm != null) {
+                for (i in 0..rawIdm.size - 1) {
+                    idmByte.append(Integer.toHexString(rawIdm[i].toInt()))
+                }
+                idm = idmByte.toString()
+            }
+            return idm
+        }
+
+        @Throws(UnsupportedEncodingException::class)
+        private fun readText(record: NdefRecord): String {
+            val payload = record.payload
+            val languageCodeLength = 4
+            return String(payload, languageCodeLength + 1, payload.size - languageCodeLength - 8, "UTF-8")
         }
     }
 
 }
+
